5e62d156ccdbf2a4a91a24458ef4c0bb5ddbf497
jonathanvdv <70983783+jonathanvdv@users.noreply.github.com>
2020-12-06 21:49:31 -0500

    Lots of New Updates
    - Created quiz directory
    - Created support form front-end (Backend work in progress)
    - Minor UI updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Navbar from './components/layout/SideNavbar'
-import Dashboard from './components/dashboard/Dashboard'
-import ArticleDetails from './components/articles/ArticleDetails'
-import SignIn from './components/auth/SignIn'
-import SignUp from './components/auth/SignUp'
-import PHQ9 from './components/quiz/PHQ9/PHQ9'
-import GAD7 from './components/quiz/GAD7/GAD7'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/SideNavbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ArticleDetails from './components/articles/ArticleDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import PHQ9 from './components/quiz/PHQ9/PHQ9';
+import GAD7 from './components/quiz/GAD7/GAD7';
 import MyLibrary from './components/articles/MyLibrary';
 import AtlasLibrary from './components/articles/AtlasLibrary';
+import quizdirectory from './components/quiz/quizdirectory';
+import support from './components/support/support';
 
 class App extends Component { 
   render(){ 
@@ -23,8 +25,11 @@ class App extends Component {
             <Route path = '/signup' component = { SignUp }/>
             <Route path = '/atlas-library' component = { AtlasLibrary }/>
             <Route path = '/my-library' component = { MyLibrary }/>
-            <Route path = '/phq9' component = { PHQ9 }/>
-            <Route path = '/gad7' component = { GAD7 }/>
+            <Route path = '/quizdirectory' component = { quizdirectory }/>
+            <Route path = '/support' component = { support }/>
+            <Route path = '/GAD7' component = { GAD7 }/>
+            <Route path = '/PHQ9' component = { PHQ9 }/>
+            
           </Switch>
         </div>
       </BrowserRouter>
