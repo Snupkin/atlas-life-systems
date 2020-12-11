391d7974150727969bc258bb7e3e65f45291935a
jonathanvdv <70983783+jonathanvdv@users.noreply.github.com>
2020-12-10 19:01:44 -0500

    images

--- src/components/quiz/quizdirectory.js
+++ src/components/quiz/quizdirectory.js
@@ -8,14 +8,16 @@ class quizdirectory extends React.Component {
             <div class="container">
                 <div class="col s12 m6">
                     <div class="card z-depth-1">
-                        <div class="card-content white-text">
-                            <span class="card-title blue-text text-darken-3">Generalized Anxiety Disorder 7</span>
-                            <h5 class='grey-text'>Screening and monitoring quiz to measure the severity and progression of depression symptoms</h5>
-                            <p class='grey-text'>The Patient Health Questionnaire 9 measures depression symptoms using the DSM-5 criteria and scores 
-                            each symptom by presence in the past two weeks ranging from 0 (not at all) to 3 (nearly everyday. </p>
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="./src/images/Quiz1.png" alt="this is a test" />
                         </div>
-                        <div class="card-action">
-                            <NavLink class='waves-effect waves-light btn white-text' to="/GAD-7">Take the Quiz</NavLink>  
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Generalized Anxiety Disorder 7<i class="material-icons right">more_vert</i></span>
+                            <p><a href="#">This is a link</a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Generalized Anxiety Disorder 7<i class="material-icons right">close</i></span>
+                            <p>The Patient Health Questionnaire 9 measures depression symptoms using the DSM-5 criteria and scores each symptom by presence in the past two weeks ranging from 0 (not at all) to 3 (nearly everyday).</p>
                         </div>
                     </div>
                 </div>
